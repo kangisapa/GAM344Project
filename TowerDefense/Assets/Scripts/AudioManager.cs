93d0746cb526e59d059074f3f2741fdc2a7b422c
IoInBetween0 <cmastin@depaul.edu>
2026-05-02 20:11:43 -0500

    Change to scripts
    Following a yt tutorial now to help

--- TowerDefense/Assets/Scripts/AudioManager.cs
+++ TowerDefense/Assets/Scripts/AudioManager.cs
@@ -1,7 +1,28 @@
+using System;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
-}
+
+    public void PlaySFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s != null) 
+        {
+
+            Debug.Log("SFX Not Found");
+        
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.clip);
+        }
+
+
+
+    }
+    }
