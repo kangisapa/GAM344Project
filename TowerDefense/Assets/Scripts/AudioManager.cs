008c9b7f7e678ea1574e9de1456eb1acea54a339
IoInBetween0 <cmastin@depaul.edu>
2026-05-02 20:18:58 -0500

    Update AudioManager.cs
    Changes to sound script chnage from != to ==

--- TowerDefense/Assets/Scripts/AudioManager.cs
+++ TowerDefense/Assets/Scripts/AudioManager.cs
@@ -3,14 +3,31 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager Instance;
+
     public Sound[] musicSounds, sfxSounds;
     public AudioSource musicSource, sfxSource;
 
+
+    private void Awake()
+    {
+       if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+       else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //put in here play music on start later when we have music
     public void PlaySFX(string name)
     {
         Sound s = Array.Find(sfxSounds, x => x.name == name);
 
-        if (s != null) 
+        if (s == null) 
         {
 
             Debug.Log("SFX Not Found");
@@ -21,8 +38,6 @@ public class AudioManager : MonoBehaviour
         {
             sfxSource.PlayOneShot(s.clip);
         }
-
-
 
     }
     }
